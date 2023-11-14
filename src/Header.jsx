fffdda2da76bee15bc7c8a24a88c924862053698
Piggey <pepcio3320@gmail.com>
2023-11-14 20:46:12 +0100

    projekcik cykniety essa

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import { AppBar, Toolbar, CameraIcon, Typography } from "@mui/material"
+export default function Header() {
+  return (
+    <AppBar position="relative">
+      <Toolbar>
+        <CameraIcon sx={{ mr: 2 }} />
+        <Typography variant="h6" color="inherit" noWrap>
+          Album layout
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  )
+}
