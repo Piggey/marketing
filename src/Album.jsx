990a75b5a81fec310e9e7218b5bed3147dcf4084
Piggey <pepcio3320@gmail.com>
2023-11-14 20:57:17 +0100

    podjebany projekt dziala

--- src/Album.jsx
+++ src/Album.jsx
@@ -1,44 +1,54 @@
-import {
-  CssBaseline,
-  Typography,
-  Box,
-  Container,
-  Stack,
-  Grid,
-  Button,
-  Card,
-  CardMedia,
-  CardContent,
-  CardActions,
-  ThemeProvider,
-  createTheme,
-} from "@mui/material";
-import Header from "./Header";
-import Footer from "./Footer";
+import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
+import CameraIcon from '@mui/icons-material/PhotoCamera';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#ffffff", // biały jako główny kolor
-    },
-    secondary: {
-      main: "#8b0000", // dark red
-    },
-  },
-});
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+const defaultTheme = createTheme();
+
 export default function Album() {
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={defaultTheme}>
       <CssBaseline />
-      <Header />
+      <AppBar position="relative">
+        <Toolbar>
+          <CameraIcon sx={{ mr: 2 }} />
+          <Typography variant="h6" color="inherit" noWrap>
+            Album layout
+          </Typography>
+        </Toolbar>
+      </AppBar>
       <main>
         {/* Hero unit */}
         <Box
           sx={{
-            bgcolor: "background.paper",
+            bgcolor: 'background.paper',
             pt: 8,
             pb: 6,
           }}
@@ -53,15 +63,10 @@ export default function Album() {
             >
               Album layout
             </Typography>
-            <Typography
-              variant="h5"
-              align="center"
-              color="text.secondary"
-              paragraph
-            >
-              Something short and leading about the collection below—its
-              contents, the creator, etc. Make it short and sweet, but not too
-              short so folks don&apos;t simply skip over it entirely.
+            <Typography variant="h5" align="center" color="text.secondary" paragraph>
+              Something short and leading about the collection below—its contents,
+              the creator, etc. Make it short and sweet, but not too short so folks
+              don&apos;t simply skip over it entirely.
             </Typography>
             <Stack
               sx={{ pt: 4 }}
@@ -80,17 +85,13 @@ export default function Album() {
             {cards.map((card) => (
               <Grid item key={card} xs={12} sm={6} md={4}>
                 <Card
-                  sx={{
-                    height: "100%",
-                    display: "flex",
-                    flexDirection: "column",
-                  }}
+                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
                 >
                   <CardMedia
                     component="div"
                     sx={{
                       // 16:9
-                      pt: "56.25%",
+                      pt: '56.25%',
                     }}
                     image="https://source.unsplash.com/random?wallpapers"
                   />
@@ -99,8 +100,8 @@ export default function Album() {
                       Heading
                     </Typography>
                     <Typography>
-                      This is a media card. You can use this section to describe
-                      the content.
+                      This is a media card. You can use this section to describe the
+                      content.
                     </Typography>
                   </CardContent>
                   <CardActions>
@@ -113,7 +114,22 @@ export default function Album() {
           </Grid>
         </Container>
       </main>
-      <Footer />
+      {/* Footer */}
+      <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="text.secondary"
+          component="p"
+        >
+          Something here to give the footer a purpose!
+        </Typography>
+        <Copyright />
+      </Box>
+      {/* End footer */}
     </ThemeProvider>
   );
 }
