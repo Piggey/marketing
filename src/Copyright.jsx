ab53a8817fe89d797eb7a897e5bb5274a7d287d3
Piggey <pepcio3320@gmail.com>
2023-11-17 21:57:53 +0100

    my copyright

--- src/Copyright.jsx
+++ src/Copyright.jsx
@@ -4,8 +4,8 @@ export default function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
+      <Link color="inherit" href="https://ftims.edu.p.lodz.pl/user/profile.php?id=20396">
+        Dawid Sośnia
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
