6599973dbac87ca5cca3e780ed966438b9ac2860
Piggey <pepcio3320@gmail.com>
2023-11-17 23:58:32 +0100

    mock team data

---
+++ src/data/team.js
@@ -0,0 +1,38 @@
+export const team = [
+  {
+    imgSrc: "src/assets/1.jpeg",
+    name: "Amanda Bąk",
+    description: "Radca prawny",
+    phoneNumber: "+48 24 806 37 65",
+  },
+  {
+    imgSrc: "src/assets/2.jpeg",
+    name: "Mikołaj Włodarczyk",
+    description: "Radca prawny",
+    phoneNumber: "+48 24 806 37 66",
+  },
+  {
+    imgSrc: "src/assets/3.jpeg",
+    name: "Regina Zielińska",
+    description: "Adwokat",
+    phoneNumber: "+48 24 806 37 67",
+  },
+  {
+    imgSrc: "src/assets/4.jpeg",
+    name: "Anatol Pietrzak",
+    description: "Prawnik, adwokat",
+    phoneNumber: "+48 24 806 37 68",
+  },
+  {
+    imgSrc: "src/assets/5.jpeg",
+    name: "Kacper Chmielewski",
+    description: "Partner, radca prawny",
+    phoneNumber: "+48 24 806 37 69",
+  },
+  {
+    imgSrc: "src/assets/6.jpeg",
+    name: "Norbert Jankowski",
+    description: "Starszy prawnik, adwokat",
+    phoneNumber: "+48 24 806 37 70",
+  },
+];
