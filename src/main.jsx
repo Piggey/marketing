0417ffc328e5df5d733677f89c062c791fabb3d9
Piggey <pepcio3320@gmail.com>
2023-11-17 22:44:29 +0100

    set pages up

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import Office from './Office'
+import About from './About'
+import Contact from './Contact'
+import News from './News'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/office",
+    element: <Office />,
+  },
+  {
+    path: "/news",
+    element: <News />
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
   },
 ]);
 
