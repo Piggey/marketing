2d306d7c6bb12df47e4cfb0609350696d39e7a07
Piggey <pepcio3320@gmail.com>
2023-11-17 22:35:27 +0100

    add buttons to pages

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,13 +1,29 @@
-import { AppBar, Toolbar, Typography } from "@mui/material"
+import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <AppBar position="relative">
       <Toolbar>
         <Typography variant="h6" color="inherit" noWrap>
-          Album layout
+          <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
+            Kancelaria Prawna
+          </Link>
         </Typography>
+        <Box sx={{ flexGrow: 1 }} />
+        <Button component={Link} to="/about" color="inherit">
+          O Nas
+        </Button>
+        <Button component={Link} to="/office" color="inherit">
+          Kancelaria
+        </Button>
+        <Button component={Link} to="/contact" color="inherit">
+          Kontakt
+        </Button>
+        <Button component={Link} to="/news" color="inherit">
+          Aktualności
+        </Button>
       </Toolbar>
     </AppBar>
-  )
+  );
 }
