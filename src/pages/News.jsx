de0a73b8d633d2713eadacd74e3c1125f6629b99
Piggey <pepcio3320@gmail.com>
2023-11-18 01:11:33 +0100

    display articles

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,5 +1,13 @@
+import { Grid } from "@mui/material";
+import Article from "../components/Article";
+import { articles } from "../data/articles";
+
 export default function News() {
   return (
-    <div>Aktualności</div>
+    <Grid container spacing={5} sx={{ mt: 3, ml: 5 }}>
+      {articles.map((article) => (
+        <Article key={article} author={article.author} title={article.title} content={article.content} date={article.date} tags={article.tags} />
+      ))}
+    </Grid>
   );
 }
