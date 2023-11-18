397488b779fbfc31e12dcc7e4e58215681c76b34
Piggey <pepcio3320@gmail.com>
2023-11-18 01:11:16 +0100

    mock articles

---
+++ src/data/articles.js
@@ -0,0 +1,37 @@
+export const articles = [
+  {
+    title: "Podstawowe kroki do założenia firmy",
+    content: "W tym artykule omówimy kluczowe kroki, jakie należy podjąć przy zakładaniu własnej firmy. Zaczniemy od wyboru formy prawnej, a skończymy na rejestracji.",
+    date: "2023-11-17",
+    tags: ["prawo firmowe", "zakładanie firmy", "rejestracja firmowa"],
+    author: "Anna Kowalska",
+  },
+  {
+    title: "Nowelizacje w prawie pracy - co się zmienia?",
+    content: "Artykuł ten przybliży najnowsze zmiany w prawie pracy. Dowiesz się, jakie nowelizacje weszły w życie i jak wpłyną na pracodawców oraz pracowników.",
+    date: "2023-11-18",
+    tags: ["prawo pracy", "nowelizacje", "zmiany prawne"],
+    author: "Piotr Nowak",
+  },
+  {
+    title: "Ochrona danych osobowych w firmie",
+    content: "Zapoznaj się z zasadami ochrony danych osobowych w Twojej firmie. Dowiesz się, jak prawidłowo gromadzić, przetwarzać i chronić dane klientów zgodnie z obowiązującymi przepisami.",
+    date: "2023-11-19",
+    tags: ["RODO", "ochrona danych osobowych", "prawo prywatności"],
+    author: "Janina Wiśniewska",
+  },
+  {
+    title: "Spory sądowe w biznesie - jak unikać i jak się bronić?",
+    content: "Przeczytaj ten artykuł, aby dowiedzieć się, jak unikać sporów sądowych w prowadzeniu biznesu oraz jak skutecznie bronić się, gdy do takiego sporu dojdzie.",
+    date: "2023-11-20",
+    tags: ["spory sądowe", "prawo cywilne", "rozwiązywanie sporów"],
+    author: "Marcin Kowalczyk",
+  },
+  {
+    title: "Prawo autorskie w internecie - co warto wiedzieć?",
+    content: "Internet to przestrzeń, gdzie prawa autorskie odgrywają istotną rolę. Przeczytaj ten artykuł, aby zrozumieć, jak chronić swoje prawa autorskie online.",
+    date: "2023-11-21",
+    tags: ["prawo autorskie", "internet", "ochrona własności intelektualnej"],
+    author: "Ewa Nowakowska",
+  },
+];
