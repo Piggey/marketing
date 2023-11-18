076d14c49ec1bacfc0820a192d3d2b0caa5b8559
Piggey <pepcio3320@gmail.com>
2023-11-18 01:11:23 +0100

    article component

---
+++ src/components/Article.jsx
@@ -0,0 +1,24 @@
+import { Grid, Typography, Divider } from "@mui/material";
+import React from "react";
+
+/* eslint-disable react/prop-types */
+export default function Article({ author, title, content, date, tags }) {
+  return (
+    <Grid item xs={12} md={8}>
+      <Typography variant="h4" gutterBottom>
+        {title}
+      </Typography>
+      <Divider />
+      <Typography variant="h6">{`${date} - ${author}`}</Typography>
+      {tags.map((tag, index) => (
+        <React.Fragment key={tag}>
+          <Typography color={"primary"} sx={{ display: "inline" }}>{tag}</Typography>
+          {index < tags.length - 1 && <span>, </span>}
+        </React.Fragment>
+      ))}
+      <Divider />
+      <Typography justifyContent={true}>{content}</Typography>
+      <Divider />
+    </Grid>
+  );
+}
