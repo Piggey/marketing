abe1a236732174ac2b0f95b404d3f77b93d6f57d
Piggey <pepcio3320@gmail.com>
2023-11-18 18:06:58 +0100

    hero descriptions for pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,7 @@ import Hero from "../components/Hero";
 export default function About() {
   return (
     <>
-      <Hero imgSrc="src/assets/about.jpeg" imgYOffset="26%" title="O nas" description="Lorem Ipsum huj w dupe" />
+      <Hero imgSrc="src/assets/about.jpeg" imgYOffset="26%" title="O nas" description="Poznaj nasz zespół" />
       <Container sx={{ py: 8 }}>
         <Grid container spacing={4}>
           {team.map((member) => (
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,7 @@ import Hero from "../components/Hero";
 export default function Contact() {
   return (
     <>
-      <Hero imgSrc="src/assets/contact.jpg" title="Kontakt" description="Lorem Ipsum huj w dupe" />
+      <Hero imgSrc="src/assets/contact.jpg" title="Kontakt" />
       <div>Kontakt</div>
     </>
   );
